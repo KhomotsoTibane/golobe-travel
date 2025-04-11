5b3121513f9838ad8706234c2a34a91ce507be32
Khomotso <khomotso13@gmail.com>
2025-04-11 17:28:54 +0200

    feat: add user favorites page

--- src/routes/_authenticated/profile.tsx
+++ src/routes/_authenticated/profile.tsx
@@ -1,9 +1,134 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useRouteContext } from "@tanstack/react-router";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Pencil, UploadCloud } from "lucide-react";
 
-export const Route = createFileRoute('/_authenticated/profile')({
+import { AccountBg } from "@/assets/images";
+
+export const Route = createFileRoute("/_authenticated/profile")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_authenticated/profile"!</div>
+  const { queryClient, user } = useRouteContext({ from: "/_authenticated" });
+
+  const name = user?.userDetails.name;
+  const email = user?.userDetails.email;
+  return (
+    <div className="max-w-4xl mx-auto mt-10 p-4 border">
+      {/* Cover Image */}
+      <div
+        className="relative h-48 w-full rounded-xl  border border-accent-300"
+        style={{
+          backgroundRepeat: "no-repeat",
+          backgroundImage: `url(${AccountBg})`,
+          objectFit: "contain",
+          backgroundSize: "cover",
+        }}
+      >
+        <Button
+          variant={"default"}
+          className="absolute bottom-2 right-2 flex items-center gap-2 text-black"
+        >
+          <UploadCloud className="w-4 h-4" /> Upload new cover
+        </Button>
+
+        {/* Profile Picture */}
+        <div className="absolute  -bottom-10 left-1/2 -translate-x-1/2 ">
+          <div className="relative w-20 h-20 rounded-full border-4 border-white overflow-hidden">
+            <img
+              src="https://randomuser.me/api/portraits/men/1.jpg"
+              alt="profile"
+              className="w-full h-full object-cover"
+            />
+            <Button
+              size="icon"
+              variant="secondary"
+              className="absolute bottom-0 right-0 w-6 h-6 rounded-full p-0"
+            >
+              <Pencil className="w-3 h-3" />
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {/* User Info */}
+      <div className="mt-16 text-center">
+        <h5 className="">{name}</h5>
+        <p className="">{email}</p>
+      </div>
+
+      {/* Tabs */}
+      <Tabs defaultValue="account" className="mt-10 w-full">
+        <TabsList className="flex justify-center w-full h-20 bg-white card-shadow">
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="history">History</TabsTrigger>
+          <TabsTrigger value="payments">Payment methods</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="account">
+          <Card className="p-6 space-y-6 mt-6 bg-white border-none card-shadow">
+            <div className="flex justify-between items-center">
+              <div>
+                <p className="text-sm text-muted-foreground">Name</p>
+                <p>{name}</p>
+              </div>
+              <Button disabled variant="outline">
+                Change
+              </Button>
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div>
+                <p className="text-sm text-muted-foreground">Email</p>
+                <p>{email}</p>
+              </div>
+              <div className="flex gap-2">
+                <Button disabled variant="outline">
+                  Change
+                </Button>
+              </div>
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div>
+                <p className="text-sm text-muted-foreground">Phone number</p>
+                <p></p>
+              </div>
+              <Button variant="outline">Change</Button>
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div>
+                <p className="text-sm text-muted-foreground">Address</p>
+                <p></p>
+              </div>
+              <Button variant="outline">Change</Button>
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div>
+                <p className="text-sm text-muted-foreground">Date of birth</p>
+                <p></p>
+              </div>
+              <Button variant="outline">Change</Button>
+            </div>
+          </Card>
+        </TabsContent>
+
+        <TabsContent value="history">
+          <Card className="p-6 mt-6 text-sm text-muted-foreground bg-white border-none">
+            Booking and travel history goes here.
+          </Card>
+        </TabsContent>
+
+        <TabsContent value="payments">
+          <Card className="p-6 mt-6 text-sm text-muted-foreground bg-white border-none">
+            Payment methods and billing info go here.
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
